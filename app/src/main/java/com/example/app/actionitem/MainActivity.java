2adbf4fc4591cbed1297f90b244e43935acf96da
Muthuraja <muthuraja.g@newgendigital.com>
2018-10-15 15:32:46 +0530

    First Commit

--- app/src/main/java/com/example/app/actionitem/MainActivity.java
+++ app/src/main/java/com/example/app/actionitem/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
